d8b5a44f917b6cdb5ffd4a5c3657475b65b263b9
informix <eric.vercelletto@begooden-it.com>
2016-06-02 18:42:41 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# ReceiverMqtt
+# ReceiverMqtt
+# ReceiverMqtt
---
+++ constants.py
@@ -0,0 +1,10 @@
+public abstract class K {
+	public static final String CLIENT_IP = "clientIp";
+	public static final String DATE_FORMAT = "yyyy-MM-dd'T'HH:mm:ss.S";
+	public static final String FORMAT_VERSION = "formatVersion";
+	public static final String ID = "id";
+	public static final String NAME = "name";
+	public static final String NETWORK = "network";
+	public static final String QUEUE_PATH = "/Queue/";
+	public static final String TS_RECEIVER = "tsReceiver";
+}
