4305f936fd242c655542ac59196b5568241f09eb
informix <eric.vercelletto@begooden-it.com>
2016-06-05 16:56:57 +0200

    V1 operational

---
+++ OpenMessageReceiver.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+import paho.mqtt.client as mqtt
+import time
+import json
+import pymongo
+
+
+# grant_token: returns the token. Please take your wonderful  funtion
+def grant_token():
+	__token = "[t23456789012345678901234567890123]"
+	return __token
+
+
+# The callback for when the client receives a CONNACK response from the server.
+def on_connect(client, userdata, rc):
+	print("Connected with result code "+str(rc))
+	# Subscribing in on_connect() means that if we lose the connection and
+	# reconnect then subscriptions will be renewed.
+	client.subscribe("4iot/#")
+
+# The callback for when a PUBLISH message is received from the server.
+def on_message(client, userdata, msg):
+	if ( msg.topic == "4iot/message" ):
+		message = json.loads(msg.payload)
+		print "Topic: ", msg.topic+"\nMessage: "+json.dumps(msg.payload)
+		#print "Topic: ", msg.topic+"\nMessage: "+ message
+		iotdb.messages.insert(message)
+	elif ( msg.topic == "4iot/please_register" ):
+		# register claim from new client
+		requester = str(msg.payload)
+		print "this is a register request from " + requester
+		Token = grant_token ()
+		time.sleep(10)
+		grant_topic = "4iot/register_accepted/" + requester
+		grant_response = "device is now identified as " + Token
+		client.publish(grant_topic,grant_response)
+	
+		
+
+conn=pymongo.MongoClient('158.69.160.58',27017):
+iotdb=conn.iot_staging
+client = mqtt.Client()
+client.on_connect = on_connect
+client.on_message = on_message
+
+client.connect("192.168.1.30", 8883, 60)
+# Blocking call that processes network traffic, dispatches callbacks and
+# handles reconnecting.
+# Other loop*() functions are available that give a threaded interface and a
+# manual interface.
+client.loop_forever()
